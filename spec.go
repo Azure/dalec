5704efce4b280ad78f529901ccd94124b62430a5
Brian Goff <cpuguy83@gmail.com>
2023-09-25 16:55:51 +0000

    Add struct tags to all the things in Spec
    This also prepares for generating jsonschema for editor integration.

--- spec.go
+++ spec.go
@@ -1,3 +1,4 @@
+//go:generate go run ./cmd/gen-jsonschema ./spec.schema.json
 package dalec
 
 import (
@@ -10,30 +11,30 @@ import (
 // Spec is the specification for a package build.
 type Spec struct {
 	// Name is the name of the package.
-	Name string
+	Name string `yaml:"name" json:"name" jsonschema:"required"`
 	// Description is a short description of the package.
-	Description string
+	Description string `yaml:"description" json:"description" jsonschema:"required"`
 	// Website is the URL to store in the metadata of the package.
-	Website string
+	Website string `yaml:"website" json:"website"`
 
-	Version  string
-	Revision string
+	Version  string `yaml:"version" json:"version" jsonschema:"required"`
+	Revision string `yaml:"revision" json:"revision" jsonschema:"required"`
 
 	// Marks the package as architecture independent.
 	// It is up to the package author to ensure that the package is actually architecture independent.
 	// This is metadata only.
-	NoArch bool `yaml:"noarch"`
+	NoArch bool `yaml:"noarch" json:"noarch"`
 
 	// Conflicts is the list of packages that conflict with the generated package.
 	// This will prevent the package from being installed if any of these packages are already installed or vice versa.
-	Conflicts map[string][]string
+	Conflicts map[string][]string `yaml:"conflicts,omitempty" json:"conflicts,omitempty"`
 	// Replaces is the list of packages that are replaced by the generated package.
-	Replaces map[string][]string
+	Replaces map[string][]string `yaml:"replaces,omitempty" json:"replaces,omitempty"`
 	// Provides is the list of things that the generated package provides.
 	// This can be used to satisfy dependencies of other packages.
 	// As an example, the moby-runc package provides "runc", other packages could depend on "runc" and be satisfied by moby-runc.
 	// This is an advanced use case and consideration should be taken to ensure that the package actually provides the thing it claims to provide.
-	Provides []string
+	Provides []string `yaml:"provides,omitempty" json:"provides,omitempty"`
 
 	// Sources is the list of sources to use to build the artifact(s).
 	// The map key is the name of the source and the value is the source configuration.
@@ -41,46 +42,46 @@ type Spec struct {
 	// This can be mounted into the build using the "Mounts" field in the StepGroup.
 	//
 	// Sources can be embedded in the main spec as here or overriden in a build request.
-	Sources map[string]Source
+	Sources map[string]Source `yaml:"sources,omitempty" json:"sources,omitempty"`
 
 	// Patches is the list of patches to apply to the sources.
 	// The map key is the name of the source to apply the patches to.
 	// The value is the list of patches to apply to the source.
 	// The patch must be present in the `Sources` map.
 	// Each patch is applied in order and the result is used as the source for the build.
-	Patches map[string][]string
+	Patches map[string][]string `yaml:"patches,omitempty" json:"patches,omitempty"`
 
 	// Build is the configuration for building the artifacts in the package.
-	Build ArtifactBuild
+	Build ArtifactBuild `yaml:"build,omitempty" json:"build,omitempty"`
 
 	// Args is the list of arguments that can be used for shell-style expansion in (certain fields of) the spec.
 	// Any arg supplied in the build request which does not appear in this list will cause an error.
 	// Attempts to use an arg in the spec which is not specified here will assume to be a literal string.
 	// The map value is the default value to use if the arg is not supplied in the build request.
-	Args map[string]string
+	Args map[string]string `yaml:"args,omitempty" json:"args,omitempty"`
 
-	License  string
-	Vendor   string
-	Packager string
+	License  string `yaml:"license" json:"license"`
+	Vendor   string `yaml:"vendor" json:"vendor"`
+	Packager string `yaml:"packager" json:"packager"`
 
 	// Artifacts is the list of artifacts to include in the package.
-	Artifacts Artifacts
+	Artifacts Artifacts `yaml:"artifacts,omitempty" json:"artifacts,omitempty"`
 
 	// The list of distro targets to build the package for.
-	Targets map[string]Target `yaml:"targets,omitempty"`
+	Targets map[string]Target `yaml:"targets,omitempty" json:"targets,omitempty"`
 
 	// Dependencies are the different dependencies that need to be specified in the package.
 	// Dependencies are overwritten if specified in the target map for the requested distro.
-	Dependencies *PackageDependencies `yaml:"dependencies,omitempty"`
+	Dependencies *PackageDependencies `yaml:"dependencies,omitempty" json:"dependencies,omitempty"`
 	// Image is the image configuration when the target output is a container image.
 	// This is overwritten if specified in the target map for the requested distro.
-	Image *ImageConfig `yaml:"image,omitempty"`
+	Image *ImageConfig `yaml:"image,omitempty" json:"image,omitempty"`
 }
 
 type Artifacts struct {
 	// NOTE: Using a struct as a map value for future expansion
-	Binaries map[string]ArtifactConfig `yaml:"binaries"`
-	Manpages map[string]ArtifactConfig `yaml:"manpages"`
+	Binaries map[string]ArtifactConfig `yaml:"binaries" json:"binaries"`
+	Manpages map[string]ArtifactConfig `yaml:"manpages" json:"manpages"`
 	// TODO: other types of artifacts (systtemd units, libexec, configs, etc)
 	// NOTE: When other artifact types are added, you must also update [ArtifactsConfig.IsEmpty]
 }
@@ -90,10 +91,10 @@ type ArtifactConfig struct {
 	//
 	// As an example, binaries are typically placed in /usr/bin when installed.
 	// If you want to nest them in a subdirectory, you can specify it here.
-	SubPath string `yaml:"subpath"`
+	SubPath string `yaml:"subpath" json:"subpath"`
 	// Name is file or dir name to use for the artifact in the package.
 	// If empty, the file or dir name from the produced artifact will be used.
-	Name string `yaml:"name"`
+	Name string `yaml:"name" json:"name"`
 }
 
 // IsEmpty is used to determine if there are any artifacts to include in the package.
@@ -108,16 +109,16 @@ func (a *Artifacts) IsEmpty() bool {
 }
 
 type ImageConfig struct {
-	Entrypoint []string            `yaml:"entrypoint"`
-	Cmd        []string            `yaml:"cmd"`
-	Env        []string            `yaml:"env"`
-	Labels     map[string]string   `yaml:"labels"`
-	Volumes    map[string]struct{} `yaml:"volumes"`
-	WorkingDir string              `yaml:"working_dir"`
-	StopSignal string              `yaml:"stop_signal"`
+	Entrypoint []string            `yaml:"entrypoint" json:"entrypoint"`
+	Cmd        []string            `yaml:"cmd" json:"cmd"`
+	Env        []string            `yaml:"env" 	json:"env"`
+	Labels     map[string]string   `yaml:"labels" json:"labels"`
+	Volumes    map[string]struct{} `yaml:"volumes" json:"volumes"`
+	WorkingDir string              `yaml:"working_dir" json:"working_dir"`
+	StopSignal string              `yaml:"stop_signal" json:"stop_signal"`
 	// Base is the base image to use for the output image.
 	// This only affects the output image, not the build image.
-	Base string `yaml:"base"`
+	Base string `yaml:"base" json:"base"`
 }
 
 // Source defines a source to be used in the build.
@@ -125,15 +126,15 @@ type ImageConfig struct {
 type Source struct {
 	// Ref is a unique identifier for the source.
 	// example: "docker-image://busybox:latest", "https://github.com/moby/buildkit.git#master", "local://some/local/path
-	Ref string `yaml:"ref"`
+	Ref string `yaml:"ref" json:"ref" jsonschema:"required"`
 	// Path is the path to the source after fetching it based on the identifier.
-	Path string `yaml:"path"`
+	Path string `yaml:"path" json:"path"`
 
 	// Includes is a list of paths underneath `Path` to include, everything else is execluded
 	// If empty, everything is included (minus the excludes)
-	Includes []string `yaml:"includes"`
+	Includes []string `yaml:"includes" json:"includes"`
 	// Excludes is a list of paths underneath `Path` to exclude, everything else is included
-	Excludes []string `yaml:"excludes"`
+	Excludes []string `yaml:"excludes" json:"excludes"`
 
 	// Satisfies is the list of build dependencies that this source satisfies.
 	// This needs to match the name of the dependency in the
@@ -141,21 +142,21 @@ type Source struct {
 	// that are satisfied by this source.  This will cause the output packaging
 	// spec to elide the dependency from the package metadata but should include
 	// the dependency in the build source.
-	Satisfies []string `yaml:"satisfies"`
+	Satisfies []string `yaml:"satisfies,omitempty" json:"satisfies,omitempty"`
 
 	// KeepGitDir is used to keep the .git directory after fetching the source for git references.
-	KeepGitDir bool `yaml:"keep_git_dir"`
+	KeepGitDir bool `yaml:"keep_git_dir" json:"keep_git_dir"`
 
 	// Cmd is used to generate the source from a command.
 	// This is used when `Ref` is "cmd://"
 	// If ref is "cmd://", this is required.
-	Cmd *CmdSpec `yaml:"cmd,omitempty"`
+	Cmd *CmdSpec `yaml:"cmd,omitempty" json:"cmd,omitempty"`
 
 	// Build is used to generate source from a build.
 	// This is used when [Ref]` is "build://"
 	// The context for the build is assumed too be specified in after `build://` in the ref, e.g. `build://https://github.com/moby/buildkit.git#master`
 	// When nothing is specified after `build://`, the context is assumed to be the current build context.
-	Build *BuildSpec `yaml:"build,omitempty"`
+	Build *BuildSpec `yaml:"build,omitempty" json:"build,omitempty"`
 }
 
 // BuildSpec is used to generate source from a build.
@@ -163,18 +164,18 @@ type Source struct {
 type BuildSpec struct {
 	// Target specifies the build target to use.
 	// If unset, the default target is determined by the frontend implementation (e.g. the dockerfile frontend uses the last build stage as the default).
-	Target string `yaml:"target"`
+	Target string `yaml:"target" json:"target"`
 	// Args are the build args to pass to the build.
-	Args map[string]string `yaml:"args"`
+	Args map[string]string `yaml:"args" json:"args"`
 	// File is the path to the build file in the b uild context
 	// If not set the default is assumed by buildkit to be `Dockerfile` at the root of the context.
 	// This is exclusive with [Inline]
-	File string `yaml:"file"`
+	File string `yaml:"file" json:"file"`
 
 	// Inline is an inline build spec to use.
 	// This can be used to specify a dockerfile instead of using one in the build context
 	// This is exclusive with [File]
-	Inline string `yaml:"inline"`
+	Inline string `yaml:"inline" json:"inline"`
 }
 
 // PackageDependencies is a list of dependencies for a package.
@@ -182,58 +183,58 @@ type BuildSpec struct {
 // It also includes build-time dedendencies, which we'll install before running any build steps.
 type PackageDependencies struct {
 	// Build is the list of packagese required to build the package.
-	Build map[string][]string
+	Build map[string][]string `yaml:"build,omitempty" json:"build,omitempty"`
 	// Runtime is the list of packages required to install/run the package.
-	Runtime map[string][]string
+	Runtime map[string][]string `yaml:"runtime,omitempty" json:"runtime,omitempty"`
 	// Recommends is the list of packages recommended to install with the generated package.
 	// Note: Not all package managers support this (e.g. rpm)
-	Recommends map[string][]string
+	Recommends map[string][]string `yaml:"recommends,omitempty" json:"recommends,omitempty"`
 }
 
 // ArtifactBuild configures a group of steps that are run sequentially along with their outputs to build the artifact(s).
 type ArtifactBuild struct {
 	// Steps is the list of commands to run to build the artifact(s).
 	// Each step is run sequentially and will be cached accordingly depending on the frontend implementation.
-	Steps []BuildStep `yaml:"steps"`
+	Steps []BuildStep `yaml:"steps" json:"steps" jsonschema:"required"`
 	// List of CacheDirs which will be used across all Steps
-	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs"`
+	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs,omitempty" json:"cache_dirs,omitempty"`
 	// Env is the list of environment variables to set for all commands in this step group.
-	Env map[string]string
+	Env map[string]string `yaml:"env,omitempty" json:"env,omitempty"`
 }
 
 // BuildStep is used to execute a command to build the artifact(s).
 type BuildStep struct {
 	// Command is the command to run to build the artifact(s).
 	// This will always be wrapped as /bin/sh -c "<command>", or whatever the equivalent is for the target distro.
-	Command string `yaml:"command"`
+	Command string `yaml:"command" json:"command" jsonschema:"required"`
 	// CacheDirs is the list of CacheDirs which will be used for this build step.
 	// Note that this list will be merged with the list of CacheDirs from the StepGroup.
-	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs"`
+	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs,omitempty" json:"cache_dirs,omitempty"`
 	// Env is the list of environment variables to set for the command.
-	Env map[string]string `yaml:"env"`
+	Env map[string]string `yaml:"env,omitempty" json:"env,omitempty"`
 }
 
 type SourceMount struct {
 	// Path is the destination directory to mount to
-	Path string `yaml:"path"`
+	Path string `yaml:"path" json:"path" jsonschema:"required"`
 	// Copy is used to copy the source into the destination directory rather than mount it
-	Copy bool `yaml:"copy"`
+	Copy bool `yaml:"copy" json:"copy"`
 	// Spec specifies the source to mount
-	Spec Source `yaml:"spec"`
+	Spec Source `yaml:"spec" json:"spec" jsonschema:"required"`
 }
 
 type CmdSpec struct {
 	// Dir is the working directory to run the command in.
-	Dir string `yaml:"dir"`
+	Dir string `yaml:"dir,omitempty" json:"dir,omitempty"`
 	// Sources is the list of sources to mount into the build steps.
-	Sources []SourceMount `yaml:"sources"`
+	Sources []SourceMount `yaml:"sources,omitempty" json:"sources,omitempty"`
 
 	// List of CacheDirs which will be used across all Steps
-	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs"`
+	CacheDirs map[string]CacheDirConfig `yaml:"cache_dirs,omitempty" json:"cache_dirs,omitempty"`
 	// Env is the list of environment variables to set for all commands in this step group.
-	Env map[string]string
+	Env map[string]string `yaml:"env,omitempty" json:"env,omitempty"`
 
-	Steps []*BuildStep `yaml:"steps"`
+	Steps []*BuildStep `yaml:"steps" json:"steps" jsonschema:"required"`
 }
 
 // CacheDirConfig configures a persistent cache to be used across builds.
@@ -241,18 +242,18 @@ type CacheDirConfig struct {
 	// Mode is the locking mode to set on the cache directory
 	// values: shared, private, locked
 	// default: shared
-	Mode string `yaml:"mode"`
+	Mode string `yaml:"mode,omitempty" json:"mode,omitempty"`
 	// Key is the cache key to use to cache the directory
 	// default: Value of `Path`
-	Key string `yaml:"key"`
+	Key string `yaml:"key,omitempty" json:"key,omitempty"`
 	// IncludeDistroKey is used to include the distro key as part of the cache key
 	// What this key is depends on the frontend implementation
 	// Example for Debian Buster may be "buster"
-	IncludeDistroKey bool `yaml:"include_distro_key"`
+	IncludeDistroKey bool `yaml:"include_distro_key" json:"include_distro_key,omitempty"`
 	// IncludeArchKey is used to include the architecture key as part of the cache key
 	// What this key is depends on the frontend implementation
 	// Frontends SHOULD use the buildkit platform arch
-	IncludeArchKey bool `yaml:"include_arch_key"`
+	IncludeArchKey bool `yaml:"include_arch_key" json:"include_arch_key,omitempty"`
 }
 
 func knownArg(key string) bool {
@@ -318,23 +319,23 @@ type Frontend struct {
 	//
 	// If the original frontend does not have builtin support for the distro, this must be specified or the build will fail.
 	// If this is specified then it MUST be used.
-	Image string `yaml:"image,omitempty"`
+	Image string `yaml:"image,omitempty" json:"image,omitempty" jsonschema:"required"`
 	// CmdLine is the command line to use to forward the build to the frontend.
 	// By default the frontend image's entrypoint/cmd is used.
-	CmdLine string `yaml:"cmdline,omitempty"`
+	CmdLine string `yaml:"cmdline,omitempty" json:"cmdline,omitempty"`
 }
 
 // Target defines a distro-specific build target.
 // This is used in [Spec] to specify the build target for a distro.
 type Target struct {
 	// Dependencies are the different dependencies that need to be specified in the package.
-	Dependencies *PackageDependencies `yaml:"dependencies,omitempty"`
+	Dependencies *PackageDependencies `yaml:"dependencies,omitempty" json:"dependencies,omitempty"`
 
 	// Image is the image configuration when the target output is a container image.
-	Image *ImageConfig `yaml:"image,omitempty"`
+	Image *ImageConfig `yaml:"image,omitempty" json:"image,omitempty"`
 
 	// Frontend is the frontend configuration to use for the target.
 	// This is used to forward the build to a different, dalec-compatabile frontend.
 	// This can be useful when testing out new distros or using a different version of the frontend for a given distro.
-	Frontend *Frontend `yaml:"frontend,omitempty"`
+	Frontend *Frontend `yaml:"frontend,omitempty" json:"frontend,omitempty"`
 }
