0c1fe85585b984644da29429808ccb02120e3243
Peter Engelbert <pmengelbert@gmail.com>
2024-01-23 12:24:08 -0500

    Add unit test for fillDefaults
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- load_test.go
+++ load_test.go
@@ -1,7 +1,9 @@
 package dalec
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -145,6 +147,160 @@ func TestSourceValidation(t *testing.T) {
 			if tc.expectErr {
 				tt.Fatal("expected error, but received none")
 			}
+		})
+	}
+}
+
+func TestSourceFillDefaults(t *testing.T) {
+	cases := []struct {
+		title  string
+		before Source
+		after  Source
+	}{
+		{
+			title: "fills default context name when source type is context",
+			before: Source{
+				Context: &SourceContext{
+					Name: "",
+				},
+				Path: ".",
+			},
+			after: Source{
+				Context: &SourceContext{
+					Name: "context",
+				},
+				Path: ".",
+			},
+		},
+		{
+			title: "source.Path defaults to `.` when source type is context",
+			before: Source{
+				Context: &SourceContext{
+					Name: "context",
+				},
+				Path: "",
+			},
+			after: Source{
+				Context: &SourceContext{
+					Name: "context",
+				},
+				Path: ".",
+			},
+		},
+		{
+			title: "sets Source.Path when Source.Context.Name is nonempty",
+			before: Source{
+				Context: &SourceContext{
+					Name: "/build/test/context",
+				},
+				Path: "/the/wrong/path/you/should/never/set/this/if/you/set/context.name",
+			},
+			after: Source{
+				Context: &SourceContext{
+					Name: "/build/test/context",
+				},
+				Path: "/build/test/context",
+			},
+		},
+		{
+			title: "sets nested defaults when source type is docker image",
+			before: Source{
+				DockerImage: &SourceDockerImage{
+					Ref: "busybox:latest",
+					Cmd: &Command{
+						Dir: "/build",
+						Mounts: []SourceMount{
+							{
+								Dest: "/build/test",
+								Spec: Source{
+									Context: &SourceContext{
+										Name: "",
+									},
+									Path: ".",
+								},
+							},
+						},
+					},
+				},
+				Path: ".",
+			},
+			after: Source{
+				DockerImage: &SourceDockerImage{
+					Ref: "busybox:latest",
+					Cmd: &Command{
+						Dir: "/build",
+						Mounts: []SourceMount{
+							{
+								Dest: "/build/test",
+								Spec: Source{
+									Context: &SourceContext{
+										Name: "context",
+									},
+									Path: ".",
+								},
+							},
+						},
+					},
+				},
+				Path: ".",
+			},
+		},
+		{
+			title: "sets nested defaults when source type is build",
+			before: Source{
+				Build: &SourceBuild{
+					Inline: "FROM busybox:latest",
+					Source: Source{
+						Context: &SourceContext{
+							Name: "/build/test/context",
+						},
+						Path: "/the/wrong/path/you/should/never/set/this/if/you/set/context.name",
+					},
+				},
+				Path: ".",
+			},
+			after: Source{
+				Build: &SourceBuild{
+					Inline: "FROM busybox:latest",
+					Source: Source{
+						Context: &SourceContext{
+							Name: "/build/test/context",
+						},
+						Path: "/build/test/context",
+					},
+				},
+				Path: ".",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		title := fmt.Sprintf("source %s", tc.title)
+		t.Run(title, func(t *testing.T) {
+			src := tc.before
+			expected := tc.after
+
+			if err := src.validate(); err != nil {
+				t.Fatal(err)
+			}
+
+			fillDefaults(&src)
+
+			if !reflect.DeepEqual(src, expected) {
+				s, err := json.MarshalIndent(&src, "", "\t")
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				e, err := json.MarshalIndent(&expected, "", "\t")
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				t.Fatalf("\nactual: %s\n-------------\nexpected: %s", string(s), string(e))
+			}
+
 		})
 	}
 }
