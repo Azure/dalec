d7ae44f4cf4ff3acfa8722ec2672cfeed7e03bce
Peter Engelbert <pmengelbert@gmail.com>
2024-02-05 10:31:14 -0500

    Set up windows builder image
    Inspired by github.com/Azure/moby-packaging
    
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- frontend/windows/handle_zip.go
+++ frontend/windows/handle_zip.go
@@ -10,6 +10,15 @@ import (
 	gwclient "github.com/moby/buildkit/frontend/gateway/client"
 )
 
+const (
+	workerImgRef = "mcr.microsoft.com/mirror/docker/library/buildpack-deps:bullseye"
+)
+
+var (
+	varCacheAptMount = llb.AddMount("/var/cache/apt", llb.Scratch(), llb.AsPersistentCacheDir("dalec-windows-var-cache-apt", llb.CacheMountLocked))
+	varLibAptMount   = llb.AddMount("/var/lib/apt", llb.Scratch(), llb.AsPersistentCacheDir("dalec-windows-var-lib-apt", llb.CacheMountLocked))
+)
+
 func handleZip(ctx context.Context, client gwclient.Client, spec *dalec.Spec) (gwclient.Reference, *image.Image, error) {
 	st := llb.Scratch().File(llb.Mkfile("/hello", 0600, []byte("hello world!")))
 
@@ -27,4 +36,18 @@ func handleZip(ctx context.Context, client gwclient.Client, spec *dalec.Spec) (g
 	ref, err := res.SingleRef()
 	// Do not return a nil image, it may cause a panic
 	return ref, &image.Image{}, err
+}
+
+func workerImg(sOpt dalec.SourceOpts) llb.State {
+	// TODO: support named context override... also this should possibly be its own image, maybe?
+	return llb.Image(workerImgRef, llb.WithMetaResolver(sOpt.Resolver)).
+		Run(
+			shArgs("apt-get update && apt-get install -y build-essential binutils-mingw-w64 g++-mingw-w64-x86-64 gcc git make pkg-config quilt zip"),
+			varCacheAptMount,
+			varLibAptMount,
+		).Root()
+}
+
+func shArgs(cmd string) llb.RunOption {
+	return llb.Args([]string{"sh", "-c", cmd})
 }
