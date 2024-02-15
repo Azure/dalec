5b13547cf75c9f1ea26b46750f2c67e7399a2cfa
Peter Engelbert <pmengelbert@gmail.com>
2024-02-15 12:12:16 -0500

    save progress
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- frontend/build.go
+++ frontend/build.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/Azure/dalec"
 	"github.com/containerd/containerd/platforms"
+	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/exporter/containerimage/image"
 	"github.com/moby/buildkit/frontend/dockerui"
 	gwclient "github.com/moby/buildkit/frontend/gateway/client"
@@ -21,11 +23,29 @@ func loadSpec(ctx context.Context, client *dockerui.Client) (*dalec.Spec, error)
 	}
 
 	f := bytes.NewBuffer(bytes.TrimSpace(src.Data))
-	spec, err := dalec.LoadSpec(f)
+	specs, err := dalec.LoadSpecs(f)
 	if err != nil {
 		return nil, fmt.Errorf("error loading spec: %w", err)
 	}
-	return spec, nil
+	if len(specs) == 0 {
+		return nil, fmt.Errorf("no spec given")
+	}
+	return specs[0], nil
+}
+
+func loadSpecs(ctx context.Context, client *dockerui.Client, target string) ([]*dalec.Spec, error) {
+	src, err := client.ReadEntrypoint(ctx, "Dockerfile")
+	if err != nil {
+		return nil, fmt.Errorf("could not read spec file: %w", err)
+	}
+
+	f := bytes.NewBuffer(bytes.TrimSpace(src.Data))
+	specs, err := dalec.LoadSpecs(f)
+	if err != nil {
+		return nil, fmt.Errorf("error loading spec: %w", err)
+	}
+
+	return dalec.TopSort(specs, target)
 }
 
 func listBuildTargets(group string) []*targetWrapper {
@@ -101,19 +121,70 @@ func Build(ctx context.Context, client gwclient.Client) (*gwclient.Result, error
 		return nil, fmt.Errorf("could not create build client: %w", err)
 	}
 
-	spec, err := loadSpec(ctx, bc)
+	tgt, rest, ok := strings.Cut(bc.Target, "/")
+	if !ok {
+		return nil, fmt.Errorf("no path separator found")
+	}
+
+	_ = rest
+	specs, err := loadSpecs(ctx, bc, tgt)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := registerSpecHandlers(ctx, spec, client); err != nil {
-		return nil, err
+	for _, spec := range specs {
+		if err := registerSpecHandlers(ctx, spec, client); err != nil {
+			return nil, err
+		}
 	}
 
+	if len(specs) != 1 {
+		// do my thing
+		m := make(map[string]llb.State)
+		var last string
+		for _, spec := range specs[:len(specs)-1] {
+			var st llb.State
+			if last != "" {
+				st = m[spec.Name]
+			}
+			st, err := specToState(ctx, client, bc, spec, st)
+			if err != nil {
+				return nil, err
+			}
+			last = spec.Name
+			m[spec.Name] = st
+		}
+
+	}
+	return build(ctx, client, bc, last, m, nil)
+
+}
+
+func specToState(ctx context.Context, client gwclient.Client, bc *dockerui.Client, spec *dalec.Spec, st *llb.State) (llb.State, error) {
+	res, err := build(ctx, client, bc, spec st)
+	if err != nil {
+		return llb.Scratch(), err
+	}
+	ref, err := res.SingleRef()
+	if err != nil {
+		return llb.Scratch(), err
+	}
+	sst, err := ref.ToState()
+	if err != nil {
+		return llb.Scratch(), err
+	}
+	return sst, nil
+}
+
+func build(ctx context.Context, client gwclient.Client, bc *dockerui.Client, spec *dalec.Spec, m map[string]llb.State, st *llb.State) (*gwclient.Result, error) {
 	res, handled, err := bc.HandleSubrequest(ctx, makeRequestHandler(bc.Target))
 	if err != nil || handled {
 		return res, err
 	}
+    pst := llb.Scratch()
+    if st != nil {
+        pst = *st
+    }
 
 	if !handled {
 		// Handle additional subrequests supported by dalec
