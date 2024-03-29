4dfb0c64c11b5ae9c752295fe5c859c6c642c64b
Danny Brito <dannybrito@microsoft.com>
2024-03-29 16:32:17 +0000

    fix tdnf typo

--- frontend/mariner2/handle_depsonly.go
+++ frontend/mariner2/handle_depsonly.go
@@ -20,7 +20,7 @@ func handleDepsOnly(ctx context.Context, client gwclient.Client, spec *dalec.Spe
 	baseImg := getWorkerImage(sOpt, pg)
 	rpmDir := baseImg.Run(
 		shArgs(`set -ex; dir="/tmp/rpms/RPMS/$(uname -m)"; mkdir -p "${dir}"; tdnf install -y --releasever=2.0 --downloadonly --alldeps --downloaddir "${dir}" `+strings.Join(spec.GetRuntimeDeps(targetKey), " ")),
-		defaultTndfCacheMount(),
+		defaultTdnfCacheMount(),
 		pg,
 	).
 		AddMount("/tmp/rpms", llb.Scratch())
