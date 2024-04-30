467148036f2c800b5feea6fbb55dfba28a94faac
Peter Engelbert <pmengelbert@gmail.com>
2024-04-30 09:43:42 -0400

    Add changes to signing image before removal
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- cmd/signer/main.go
+++ cmd/signer/main.go
@@ -79,17 +79,25 @@ func main() {
 
 		signOp := ""
 		params := []ParameterKV{}
+		findPattern := llb.Scratch()
 
 		switch target {
-		case "windowscross", "windows", "mariner2":
+		case "windowscross", "windows":
 			signOp = windowsSignOp
 			params = append(params, ParameterKV{
 				ParameterName:  "CoseFlags",
 				ParameterValue: "chainunprotected",
 			})
-
-		default:
+			findPattern = base.
+				Run(llb.Args([]string{"bash", "-c",
+					`find /artifacts -type f -regextype posix-egrep regex '.*\.(exe|ps1)$' -print0 > /tmp/output/artifacts_list`})).
+				AddMount("/tmp/output/", llb.Scratch())
+		case "mariner2", "jammy":
 			signOp = linuxSignOp
+			findPattern = base.Run(llb.Args([]string{"bash", "-c",
+				`find /artifacts -type f -regextype posix-egrep regex '.*\.(rpm)$' -print0 > /tmp/output/artifacts_list`})).
+				AddMount("/tmp/output/", llb.Scratch())
+
 		}
 
 		config := Config{
@@ -117,21 +125,23 @@ func main() {
 			return nil, err
 		}
 
-		findPattern := bopts["find.pattern"]
-
 		// In order for this signing image to work, we need
 		base = base.File(llb.Mkfile("/script.sh", 0o777, []byte(`
 			#!/usr/bin/env bash
 			set -exuo pipefail
-			export KEYVAULT_NAME=$(< /run/secrets/KEYVAULT_NAME)
-			export AZURE_CLIENT_SECRET=$(< /run/secrets/AZURE_CLIENT_SECRET)
-			export AZURE_CLIENT_ID=$(< /run/secrets/AZURE_CLIENT_ID)
-			export ESRP_KEYCODE=$(< /run/secrets/ESRP_KEYCODE)
-			export AZURE_TENANT_ID=$(< /run/secrets/AZURE_TENANT_ID)
-			export BUILDER_EMAIL=$(< /run/secrets/BUILDER_EMAIL)
-			envsubst < /config_template.json > /config.json
+			: "${FIND_PATTERN}"
 
-			az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID" --allow-no-subscriptions
+			(
+				# avoid leaking secrets
+				set +x
+				envsubst < /config_template.json > /config.json
+				az login --service-principal \
+					-username "$AZURE_CLIENT_ID" \
+					-password "$AZURE_CLIENT_SECRET" \
+					--tenant "$AZURE_TENANT_ID" \
+					--allow-no-subscriptions
+			)
+			
 			readarray -d '' artifacts < <(find /artifacts -type f ${FIND_PATTERN:+-name "$FIND_PATTERN"} -print0)
 			for f in "${artifacts[@]}"; do
 				az xsign sign-file --file-name "$f" --config-file /config.json
@@ -142,12 +152,12 @@ func main() {
 
 		output := base.Run(llb.Args([]string{"bash", "/script.sh"}),
 			llb.AddEnv("FIND_PATTERN", findPattern),
-			llb.AddSecret("/run/secrets/AZURE_CLIENT_ID", llb.SecretID("AZURE_CLIENT_ID")),
-			llb.AddSecret("/run/secrets/KEYVAULT_NAME", llb.SecretID("KEYVAULT_NAME")),
-			llb.AddSecret("/run/secrets/AZURE_CLIENT_SECRET", llb.SecretID("AZURE_CLIENT_SECRET")),
-			llb.AddSecret("/run/secrets/AZURE_TENANT_ID", llb.SecretID("AZURE_TENANT_ID")),
-			llb.AddSecret("/run/secrets/ESRP_KEYCODE", llb.SecretID("ESRP_KEYCODE")),
-			llb.AddSecret("/run/secrets/BUILDER_EMAIL", llb.SecretID("BUILDER_EMAIL")),
+			secretToEnv("AZURE_CLIENT_ID"),
+			secretToEnv("KEYVAULT_NAME"),
+			secretToEnv("AZURE_CLIENT_SECRET"),
+			secretToEnv("AZURE_TENANT_ID"),
+			secretToEnv("ESRP_KEYCODE"),
+			secretToEnv("BUILDER_EMAIL"),
 		).AddMount("/artifacts", artifacts)
 
 		def, err := output.Marshal(ctx)
@@ -162,4 +172,8 @@ func main() {
 		bklog.L.WithError(err).Fatal("error running frontend")
 		os.Exit(137)
 	}
+}
+
+func secretToEnv(secretName string) llb.RunOption {
+	return llb.AddSecret(secretName, llb.SecretID(secretName), llb.SecretAsEnv(true))
 }
