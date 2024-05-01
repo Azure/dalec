007e28ecf6fd52e36c8ae6d67630f97e23f1a4f5
Peter Engelbert <pmengelbert@gmail.com>
2024-05-01 12:24:41 -0400

    Make linux distro test generic
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

---
+++ test/signing_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/Azure/dalec"
+	gwclient "github.com/moby/buildkit/frontend/gateway/client"
+)
+
+func distroSigningTest(t *testing.T, spec *dalec.Spec, buildTarget string) func(ctx context.Context, gwc gwclient.Client) (*gwclient.Result, error) {
+	return func(ctx context.Context, gwc gwclient.Client) (*gwclient.Result, error) {
+		topTgt, _, _ := strings.Cut(buildTarget, "/")
+
+		sr := newSolveRequest(withSpec(ctx, t, spec), withBuildTarget(buildTarget))
+		res, err := gwc.Solve(ctx, sr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tgt := readFile(ctx, t, "/target", res)
+		cfg := readFile(ctx, t, "/config.json", res)
+
+		if string(tgt) != topTgt {
+			t.Fatal(fmt.Errorf("target incorrect; either not sent to signer or not received back from signer"))
+		}
+
+		if !strings.Contains(string(cfg), "linux") {
+			t.Fatal(fmt.Errorf("configuration incorrect"))
+		}
+
+		return gwclient.NewResult(), nil
+	}
+}
