e643fc8b9676aae291980b736f9df4de833014db
Brian Goff <cpuguy83@gmail.com>
2024-08-02 15:42:06 -0700

    Add option to skip tests in the dalec test runner
    This is helpful when debugging things, particularly in the go tests.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- frontend/test_runner.go
+++ frontend/test_runner.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"path"
 	"path/filepath"
+	"strconv"
 	"sync"
 
 	"github.com/Azure/dalec"
@@ -18,6 +19,17 @@ import (
 
 // Run tests runs the tests defined in the spec against the given target container.
 func RunTests(ctx context.Context, client gwclient.Client, spec *dalec.Spec, ref gwclient.Reference, withTestDeps llb.StateOption, target string) error {
+	if skipVar := client.BuildOpts().Opts["build-arg:"+"DALEC_SKIP_TESTS"]; skipVar != "" {
+		skip, err := strconv.ParseBool(skipVar)
+		if err != nil {
+			return errors.Wrapf(err, "could not parse build-arg %s", "DALEC_SKIP_TESTS")
+		}
+		if skip {
+			Warn(ctx, client, llb.Scratch(), "Tests skipped due to build-arg DALEC_SKIP_TESTS="+skipVar)
+			return nil
+		}
+	}
+
 	tests := spec.Tests
 
 	t, ok := spec.Targets[target]
