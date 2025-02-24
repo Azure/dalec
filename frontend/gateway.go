e7fbf6103838aabadd8dac0a15337e377176545f
Peter Engelbert <pmengelbert@gmail.com>
2025-02-24 11:24:49 -0500

    Update unit tests to reflect new implementation
    The LLB has changed substantially since we adoped git credential
    helpers.
    
    We now check:
    - SSH sockets will be mounted
    - Secrets will be mounted as files (not provided in the environment)
    - The gomod credential helper will be mounted
    - The auth config and `go mod download` wrapper script will be mounted
    
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- frontend/gateway.go
+++ frontend/gateway.go
@@ -128,9 +128,7 @@ func SourceOptFromUIClient(ctx context.Context, c gwclient.Client, dc *dockerui.
 			return st, err
 		},
 		GitCredentialHelpers: map[string]dalec.GitCredHelperGetter{
-			dalec.GitCredentialHelperGomod: func() (llb.State, error) {
-				return GetGitCredHelper(c)
-			},
+			dalec.GitCredentialHelperGomod: GomodGitCredentialHelperGetter(c),
 		},
 	}
 }
