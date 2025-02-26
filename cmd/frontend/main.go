a54568beeeb0bd1d9ffa427e96e592a131777212
Brian Goff <cpuguy83@gmail.com>
2025-02-25 16:44:11 -0800

    Add support for Rocky and Alma Linux 8/9
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -8,7 +8,9 @@ import (
 	"github.com/Azure/dalec/frontend/debug"
 	"github.com/Azure/dalec/targets/linux/deb/debian"
 	"github.com/Azure/dalec/targets/linux/deb/ubuntu"
+	"github.com/Azure/dalec/targets/linux/rpm/almalinux"
 	"github.com/Azure/dalec/targets/linux/rpm/azlinux"
+	"github.com/Azure/dalec/targets/linux/rpm/rockylinux"
 	"github.com/Azure/dalec/targets/windows"
 	"github.com/moby/buildkit/frontend/gateway/grpcclient"
 	"github.com/moby/buildkit/util/appcontext"
@@ -37,6 +39,8 @@ func main() {
 		frontend.WithBuiltinHandler(windows.DefaultTargetKey, windows.Handle),
 		ubuntu.Handlers,
 		debian.Handlers,
+		almalinux.Handlers,
+		rockylinux.Handlers,
 		frontend.WithTargetForwardingHandler,
 	)); err != nil {
 		bklog.L.WithError(err).Fatal("error running frontend")
