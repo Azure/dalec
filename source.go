6955d931af15840532599473e73a9f4843d55d2d
Brian Goff <cpuguy83@gmail.com>
2025-05-12 19:53:58 -0700

    Reduce soem verbosity in builds
    Most of these things were here for debugging purposes when first adding
    the relevant code and is no longer needed.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- source.go
+++ source.go
@@ -650,7 +650,7 @@ func Tar(work llb.State, src llb.State, dest string, opts ...llb.ConstraintsOpt)
 	out := filepath.Join(outBase, filepath.Dir(dest))
 	worker := work.Run(
 		llb.AddMount("/src", src, llb.Readonly),
-		ShArgs("tar -C /src -cvzf /tmp/st ."),
+		ShArgs("tar -C /src -czf /tmp/st ."),
 		WithConstraints(opts...),
 	).
 		Run(
