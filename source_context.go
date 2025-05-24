9601b5ecf79258d29ca0f4e39570ea4d798617ef
Brian Goff <cpuguy83@gmail.com>
2025-05-23 17:12:54 -0700

    Create new methods for how to load a source.
    This is a step in a larger refactor to make the code dealing with
    sources easier to follow and make changes, easier to customize behavior
    for each source, and more efficient in certain cases.
    
    Nothing is using the new methods yet, this will come in a later change.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

---
+++ source_context.go
@@ -0,0 +1,80 @@
+package dalec
+
+import (
+	"context"
+
+	"github.com/moby/buildkit/client/llb"
+	"github.com/pkg/errors"
+)
+
+// SourceContext is used to generate a source from a build context. The path to
+// the build context is provided to the `Path` field of the owning `Source`.
+type SourceContext struct {
+	// Name is the name of the build context. By default, it is the magic name
+	// `context`, recognized by Docker as the default context.
+	Name string `yaml:"name,omitempty" json:"name,omitempty"`
+}
+
+func (src *SourceContext) AsState(path string, includes []string, excludes []string, sOpt SourceOpts, opts ...llb.ConstraintsOpt) (llb.State, error) {
+	if !isRoot(path) {
+		excludes = append(excludeAllButPath(path), excludes...)
+	}
+
+	st, err := sOpt.GetContext(src.Name, localIncludeExcludeMerge(includes, excludes), withFollowPath(path), withConstraints(opts))
+	if err != nil {
+		return llb.Scratch(), err
+	}
+
+	if st == nil {
+		return llb.Scratch(), errors.Errorf("context %q not found", src.Name)
+	}
+
+	return *st, nil
+}
+
+func (src *SourceContext) validate(opts fetchOptions) error {
+	if src.Name == "" {
+		// Should we assume "context" as a default?
+		return errors.New("context source must have a name")
+	}
+	return nil
+}
+
+func (src *SourceContext) IsDir() bool {
+	return true
+}
+
+func (src *SourceContext) baseState(opts fetchOptions) llb.State {
+	var lOpts []llb.LocalOption
+
+	excludes := opts.Excludes
+	if !isRoot(opts.Path) {
+		excludes = append(excludeAllButPath(opts.Path), excludes...)
+	}
+
+	lOpts = append(lOpts, localIncludeExcludeMerge(opts.Includes, excludes))
+	lOpts = append(lOpts, WithConstraints(opts.Constraints...))
+
+	return llb.Scratch().Async(func(ctx context.Context, _ llb.State, _ *llb.Constraints) (llb.State, error) {
+		st, err := opts.SourceOpt.GetContext(opts.Rename, lOpts...)
+		if err != nil {
+			return llb.Scratch(), err
+		}
+
+		if st == nil {
+			return llb.Scratch(), errors.Errorf("context %q not found", src.Name)
+		}
+		return *st, nil
+	})
+}
+
+func (src *SourceContext) toState(opts fetchOptions) llb.State {
+	st := src.baseState(opts).With(sourceFilters(opts))
+	return st
+}
+
+func (src *SourceContext) toMount(to string, opts fetchOptions, mountOpts ...llb.MountOption) llb.RunOption {
+	st := src.baseState(opts).With(mountFilters(opts))
+	mountOpts = append(mountOpts, llb.SourcePath(opts.Path))
+	return llb.AddMount(to, st, mountOpts...)
+}
