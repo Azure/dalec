c94483d6eb76f6ca0596986a42cf1b48215c1e21
Brian Chuo <brianchuo@microsoft.com>
2025-06-26 12:59:19 -0400

    Remove flag

--- generator_pip.go
+++ generator_pip.go
@@ -48,7 +48,7 @@ func withPip(g *SourceGenerator, srcSt, worker llb.State, opts ...llb.Constraint
 
 			// Build pip download command to cache dependencies
 			pipCmd := "set -e; "
-			pipCmd += "python3 -m pip download --no-binary=:all: --force-reinstall"
+			pipCmd += "python3 -m pip download --no-binary=:all:"
 
 			// Set cache directory to the mount point
 			pipCmd += " --dest=" + pipCacheDir
