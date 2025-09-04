021f0e304628ded95e44c2f4712c2931f3d5ddf3
Brian Goff <cpuguy83@gmail.com>
2025-09-03 20:44:59 -0700

    almalinux: add platform to repo cache namespace
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>
    (cherry picked from commit 71888cf64148075b1af956f48bff49303d80c4ae)
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- targets/linux/rpm/almalinux/v8.go
+++ targets/linux/rpm/almalinux/v8.go
@@ -21,6 +21,11 @@ var ConfigV8 = &distro.Config{
 
 	CacheName: dnfCacheNameV8,
 	CacheDir:  "/var/cache/dnf",
+	// Alma's repo configs do not include the $basearch variable in the mirrorlist URL
+	// This means that the cache key that dnf computes for /var/cache/dnf/<repoid>-<hash>
+	// is the same across x86_64 and aarch64, which leads to incorrect repo metadata
+	// when compiling for a non-native architecture.
+	CacheAddPlatform: true,
 
 	ReleaseVer:         "8",
 	BuilderPackages:    builderPackages,
