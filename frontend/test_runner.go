6b77f5ecf309229691ad4d6fe0a40b34667a02bc
Brian Goff <cpuguy83@gmail.com>
2025-09-12 09:36:16 -0700

    testrunner: Simplify by moving test execution inside llb.Run
    This makes it so the test evaluator runs *inside* the container.
    This means we don't need to do crazy things with stdio routing nor do we
    need to go through the buidlkit API for every file request.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- frontend/test_runner.go
+++ frontend/test_runner.go
@@ -7,10 +7,12 @@ import (
 	"path"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/Azure/dalec"
-	"github.com/google/shlex"
+	"github.com/Azure/dalec/frontend/pkg/bkfs"
+	"github.com/Azure/dalec/internal/testrunner"
 	"github.com/moby/buildkit/client/llb"
 	gwclient "github.com/moby/buildkit/frontend/gateway/client"
 	"github.com/moby/buildkit/identity"
@@ -42,11 +44,11 @@ func RunTests(ctx context.Context, client gwclient.Client, spec *dalec.Spec, ref
 		return nil
 	}
 
+	// Force evaluation here so that any errors for the build itself can surface
+	// more cleanly.
+	// Otherwise an error for something wrong in the build (e.g. a failed compilation)
+	// will look like an error in a test (or all tests).
 	if err := ref.Evaluate(ctx); err != nil {
-		// Force evaluation here so that any errors for the build itself can surface
-		// more cleanly.
-		// Otherwise an error for something wrong in the build (e.g. a failed compilation)
-		// will look like an error in a test (or all tests).
 		return err
 	}
 
@@ -60,16 +62,21 @@ func RunTests(ctx context.Context, client gwclient.Client, spec *dalec.Spec, ref
 		return err
 	}
 
-	type testPair struct {
-		st     llb.State
-		t      *dalec.TestSpec
-		stdios map[int]llb.State
-		opts   []llb.ConstraintsOpt
-	}
-
 	ctrWithDeps := ctr.With(withTestDeps)
 
-	runs := make([]testPair, 0, len(tests))
+	frontendSt, err := GetCurrentFrontend(client)
+	if err != nil {
+		// This should never happen and indicates a bug in our implementation.
+		// Nothing a user can do about it, so panic.
+		panic(err)
+	}
+
+	var group errGroup
+
+	const errorsOutputFile = "errors.txt"
+	const outputPath = "/tmp/dalec/test/output"
+	fullOutputPath := filepath.Join(outputPath, errorsOutputFile)
+
 	for _, test := range tests {
 		base := ctr
 		for k, v := range test.Env {
@@ -78,216 +85,79 @@ func RunTests(ctx context.Context, client gwclient.Client, spec *dalec.Spec, ref
 
 		var opts []llb.RunOption
 		pg := llb.ProgressGroup(identity.NewID(), "Test: "+path.Join(target, test.Name), false)
+		opts = append(opts, pg)
 
 		for _, sm := range test.Mounts {
 			opts = append(opts, sm.ToRunOption(sOpt, pg))
 		}
 
-		opts = append(opts, pg)
-		if len(test.Steps) > 0 {
-			var needsStdioMount bool
-			ios := map[int]llb.State{}
+		result := ctrWithDeps
+		result = result.File(llb.Mkdir(outputPath, 0o755, llb.WithParents(true)), pg)
 
-			worker := ctrWithDeps
-
-			for i, step := range test.Steps {
-				var stepOpts []llb.RunOption
-				id := identity.NewID()
-				ioSt := llb.Scratch()
-				if step.Stdin != "" {
-					needsStdioMount = true
-					stepOpts = append(stepOpts, llb.AddEnv("STDIN_FILE", filepath.Join("/tmp", id, "stdin")))
-					ioSt = ioSt.File(llb.Mkfile("stdin", 0444, []byte(step.Stdin)), pg)
-				}
-				if !step.Stdout.IsEmpty() {
-					needsStdioMount = true
-					stepOpts = append(stepOpts, llb.AddEnv("STDOUT_FILE", path.Join("/tmp", id, "stdout")))
-					ioSt = ioSt.File(llb.Mkfile("stdout", 0664, nil), pg)
-				}
-
-				if !step.Stderr.IsEmpty() {
-					needsStdioMount = true
-					stepOpts = append(stepOpts, llb.AddEnv("STDERR_FILE", path.Join("/tmp", id, "stderr")))
-					ioSt = ioSt.File(llb.Mkfile("stderr", 0664, nil), pg)
-				}
-
-				cmd, err := shlex.Split(step.Command)
-				if err != nil {
-					return err
-				}
-				if needsStdioMount {
-					fSt, err := client.(frontendClient).CurrentFrontend()
-					if err != nil {
-						return err
-					}
-					p := filepath.Join("/tmp", id+"-2", "dalec-redirectio")
-					stepOpts = append(stepOpts, llb.AddMount(p, *fSt, llb.SourcePath("/dalec-redirectio")))
-					cmd = append([]string{p}, cmd...)
-				}
-
-				stepOpts = append(stepOpts, llb.Args(cmd))
-				stepOpts = append(stepOpts, llb.With(func(s llb.State) llb.State {
-					for k, v := range step.Env {
-						s = s.AddEnv(k, v)
-					}
-
-					return s
-				}))
-				stepOpts = append(opts, stepOpts...)
-
-				est := worker.Run(stepOpts...)
-				if needsStdioMount {
-					ioSt = est.AddMount(filepath.Join("/tmp", id), ioSt)
-					ios[i] = ioSt
-				}
-				worker = est.Root()
-			}
-
-			runs = append(runs, testPair{st: worker, t: test, stdios: ios, opts: []llb.ConstraintsOpt{pg, dalec.Platform(platform)}})
-		} else {
-			runs = append(runs, testPair{st: base, t: test, opts: []llb.ConstraintsOpt{pg, dalec.Platform(platform)}})
-		}
-	}
-
-	var errs errorList
-	var wg sync.WaitGroup
-	for _, pair := range runs {
-		pair := pair
-		wg.Add(1)
-		go func() {
-			if err := runTest(ctx, pair.t, pair.st, pair.stdios, client, pair.opts...); err != nil {
-				errs.Append(errors.Wrap(err, "FAILED: "+path.Join(target, pair.t.Name)))
-			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-
-	return errs.Join()
-}
-
-func runTest(ctx context.Context, t *dalec.TestSpec, st llb.State, ios map[int]llb.State, client gwclient.Client, opts ...llb.ConstraintsOpt) error {
-	def, err := st.Marshal(ctx, opts...)
-	if err != nil {
-		return err
-	}
-
-	res, err := client.Solve(ctx, gwclient.SolveRequest{
-		Definition: def.ToPB(),
-		Evaluate:   true,
-	})
-	if err != nil {
-		return err
-	}
-
-	ref, err := res.SingleRef()
-	if err != nil {
-		return err
-	}
-
-	var outErr error
-	for p, check := range t.Files {
-		stat, err := ref.StatFile(ctx, gwclient.StatRequest{
-			Path: p,
-		})
-		if err != nil {
-			if check.NotExist {
-				// TODO: buildkit just gives a generic error here (with grpc code `Unknown`)
-				// There's not really a good way to determine if the error is because the file is missing or something else.
-				continue
-			}
-			return errors.Wrapf(err, "stat failed: %s", p)
+		for i, step := range test.Steps {
+			opts := append(opts, testrunner.WithTestStep(frontendSt, &step, i, fullOutputPath))
+			result = result.Run(opts...).Root()
 		}
 
-		if stat != nil && check.NotExist {
-			return errors.Errorf("file %s exists but should not", p)
+		if len(test.Files) > 0 {
+			runOpts := append(opts, testrunner.WithFileChecks(frontendSt, test, fullOutputPath))
+			result = result.Run(runOpts...).Root()
 		}
 
-		var dt []byte
-		if !check.CheckOutput.IsEmpty() {
-			dt, err = ref.ReadFile(ctx, gwclient.ReadRequest{
-				Filename: p,
-			})
+		group.Go(func() (retErr error) {
+			defer func() {
+				if r := recover(); r != nil {
+					trace := getPanicStack()
+					retErr = errors.Errorf("panic running test %q: %v\n%s", test.Name, r, trace)
+				}
+			}()
+
+			resultFS, err := bkfs.FromState(ctx, &result, client, dalec.Platform(platform))
 			if err != nil {
-				outErr = stderrors.Join(errors.Wrapf(err, "read failed: %s", p))
+				return errors.Wrap(err, "failed to run test "+test.Name)
 			}
-		}
-		if err := check.Check(string(dt), fs.FileMode(stat.Mode), stat.IsDir(), p); err != nil {
-			outErr = stderrors.Join(errors.WithStack(err))
-		}
-	}
 
-	for i, st := range ios {
-		def, err := st.Marshal(ctx, opts...)
-		if err != nil {
-			outErr = stderrors.Join(errors.Wrap(err, "failed to marshal stdio state"))
-			continue
-		}
-		res, err := client.Solve(ctx, gwclient.SolveRequest{
-			Definition: def.ToPB(),
-			Evaluate:   true,
-		})
-		if err != nil {
-			outErr = stderrors.Join(errors.Wrap(err, "failed to solve stdio state"))
-			continue
-		}
-
-		ref, err := res.SingleRef()
-		if err != nil {
-			outErr = stderrors.Join(errors.Wrap(err, "failed to get stdio ref for %d"))
-			continue
-		}
-
-		checkFile := func(c dalec.CheckOutput, name string) error {
-			if c.IsEmpty() {
+			p := strings.TrimPrefix(fullOutputPath, "/")
+			dt, err := fs.ReadFile(resultFS, p)
+			if err != nil {
+				if !stderrors.Is(err, fs.ErrNotExist) {
+					return errors.Wrapf(err, "failed to read file checks for test %q: %T", test.Name, err)
+				}
 				return nil
 			}
-			dt, err := ref.ReadFile(ctx, gwclient.ReadRequest{
-				Filename: path.Join(name),
-			})
-			if err != nil {
-				return errors.Wrapf(err, "%s: read failed", name)
-			}
-			if err := c.Check(string(dt), name); err != nil {
-				return errors.Wrap(err, name)
+			if len(dt) > 0 {
+				return errors.Errorf("test %q failed:\n%s", test.Name, string(dt))
 			}
 			return nil
-		}
-
-		step := t.Steps[i]
-		if err := checkFile(step.Stdout, "stdout"); err != nil {
-			outErr = stderrors.Join(err)
-		}
-		if err := checkFile(step.Stderr, "stderr"); err != nil {
-			outErr = stderrors.Join(err)
-		}
+		})
 	}
 
-	return outErr
+	return group.Wait()
 }
 
-type errorList struct {
-	mu sync.Mutex
-	ls []error
+type errGroup struct {
+	group sync.WaitGroup
+	mu    sync.Mutex
+	errs  []error
 }
 
-func (e *errorList) Append(err error) {
-	if err == nil {
-		return
-	}
-	e.mu.Lock()
-	e.ls = append(e.ls, err)
-	e.mu.Unlock()
+func (g *errGroup) Go(f func() error) {
+	g.group.Add(1)
+
+	go func() {
+		defer g.group.Done()
+		g.mu.Lock()
+		g.errs = append(g.errs, f())
+		g.mu.Unlock()
+	}()
 }
 
-func (e *errorList) Join() error {
-	e.mu.Lock()
-	defer e.mu.Unlock()
+func (g *errGroup) Wait() error {
+	g.group.Wait()
+	g.mu.Lock()
+	defer g.mu.Unlock()
 
-	if len(e.ls) == 0 {
-		return nil
-	}
-
-	return stderrors.Join(e.ls...)
+	err := stderrors.Join(g.errs...)
+	g.errs = g.errs[:0]
+	return err
 }
---
+++ internal/testrunner/files.go
@@ -0,0 +1,143 @@
+package testrunner
+
+import (
+	"encoding/json"
+	stderrs "errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/Azure/dalec"
+	"github.com/moby/buildkit/client/llb"
+	"github.com/pkg/errors"
+)
+
+const CheckFilesCmdName = "test-checkfiles"
+
+// CheckFilesCmd is the entrypoint for the file checking subcommand.
+// It reads the file checks from the provided file path (first argument)
+// and executes them, writing output to os.Stdout and os.Stderr.
+//
+// This should only be called from inside a container where the test is meant to run.
+func CheckFilesCmd(args []string) {
+	var files map[string]dalec.FileCheckOutput
+
+	flags := flag.NewFlagSet(CheckFilesCmdName, flag.ExitOnError)
+
+	var outputPath string
+	flags.StringVar(&outputPath, "output", "", "Path to write test results to")
+
+	if err := flags.Parse(args); err != nil {
+		fmt.Fprintln(os.Stderr, "error parsing flags:", err)
+		os.Exit(1)
+	}
+
+	if outputPath == "" {
+		fmt.Fprintln(os.Stderr, "error: output path is required")
+		os.Exit(1)
+	}
+
+	dt, err := os.ReadFile(flags.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading file checks:", err)
+		os.Exit(1)
+	}
+
+	if err := json.Unmarshal(dt, &files); err != nil {
+		fmt.Fprintln(os.Stderr, "error unmarshaling file checks:", err)
+		os.Exit(1)
+	}
+
+	if err := checkFiles(strings.Join(flags.Args()[1:], " "), files); err != nil {
+		if err := writeFileAppend(outputPath, []byte(err.Error()), 0o600); err != nil {
+			fmt.Fprintln(os.Stderr, "error writing test result:", err)
+			os.Exit(2)
+		}
+	}
+}
+
+func writeFileAppend(path string, data []byte, mode os.FileMode) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, mode)
+	if err != nil {
+		return errors.Wrapf(err, "opening file %s", path)
+	}
+	defer f.Close()
+	if _, err := f.Write(data); err != nil {
+		return errors.Wrapf(err, "writing file %s", path)
+	}
+	return nil
+}
+
+func checkFiles(testName string, files map[string]dalec.FileCheckOutput) error {
+	var errs []error
+
+	for path, check := range files {
+		if err := checkFile(path, check); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) > 0 {
+		err := stderrs.Join(errs...)
+		return err
+	}
+
+	return nil
+}
+
+func checkFile(p string, check dalec.FileCheckOutput) error {
+	stat, err := os.Lstat(p)
+	if err != nil {
+		if os.IsNotExist(err) && check.NotExist {
+			return nil
+		}
+		return errors.Wrapf(err, "checking file %s", p)
+	}
+
+	if check.NotExist {
+		return errors.Errorf("file %s exists but should not", p)
+	}
+
+	var v string
+	if !stat.IsDir() {
+		f, err := os.Open(p)
+		if err != nil {
+			return errors.Wrapf(err, "opening file %s", p)
+		}
+		defer f.Close()
+		dt, err := io.ReadAll(f)
+		if err != nil {
+			return errors.Wrapf(err, "reading file %s", p)
+		}
+		v = string(dt)
+	}
+
+	if err := check.Check(v, stat.Mode(), stat.IsDir(), p); err != nil {
+		return errors.Wrapf(err, "checking file %s", p)
+	}
+	return nil
+}
+
+// WithFileChecks returns an llb.RunOption that checks the files specified in the test spec.
+func WithFileChecks(frontend llb.State, test *dalec.TestSpec, outputPath string) llb.RunOption {
+	return dalec.RunOptFunc(func(ei *llb.ExecInfo) {
+		llb.WithCustomNamef("Check files for test %q", test.Name).SetRunOption(ei)
+
+		dt, err := json.Marshal(test.Files)
+		if err != nil {
+			ei.State = dalec.ErrorState(ei.State, fmt.Errorf("failed to marshal file checks for test %q: %w", test.Name, err))
+			llb.Args([]string{"false"}).SetRunOption(ei)
+			return
+		}
+
+		const checkFilesPath = "/tmp/dalec/internal/frontend/test/check_files"
+		llb.AddMount(checkFilesPath, frontend, llb.SourcePath("/frontend")).SetRunOption(ei)
+		st := llb.Scratch().File(llb.Mkfile("files.json", 0o600, dt))
+
+		const fileChecksPath = "/tmp/dalec/internal/frontend/test/files.json"
+		llb.AddMount(fileChecksPath, st, llb.SourcePath("files.json")).SetRunOption(ei)
+		llb.Args([]string{checkFilesPath, CheckFilesCmdName, "--output", outputPath, fileChecksPath, test.Name}).SetRunOption(ei)
+	})
+}
