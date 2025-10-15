f3890357ed254e01499d43d18d5a447fe61e5e05
mipres <mipres@microsoft.com>
2025-10-15 16:27:36 -0700

    Fixed issue with optional parameters.

--- systemd.go
+++ systemd.go
@@ -17,17 +17,17 @@ type SystemdUnitConfig struct {
 	// Name is the name systemd unit should be copied under.
 	// Nested paths are not supported. It is the user's responsibility
 	// to name the service with the appropriate extension, i.e. .service, .timer, etc.
-	Name string `yaml:"name,omitempty" json:"name"`
+	Name string `yaml:"name" json:"name"`
 
 	// Enable is used to enable the systemd unit on install
 	// This determines what will be written to a systemd preset file
-	Enable bool `yaml:"enable,omitempty" json:"enable"`
+	Enable bool `yaml:"enable" json:"enable"`
 
 	// Start can be optionally used to start the systemd unit on install
 	// This determines what will be written to a systemd preset file
 	// Note that depending on distribution there may be different
 	// expectations as to if the package should be responsible for this
-	Start bool `yaml:"start,omitempty" json:"start"`
+	Start bool `yaml:"start,omitempty" json:"start,omitempty"`
 }
 
 func (s SystemdUnitConfig) Artifact() *ArtifactConfig {
