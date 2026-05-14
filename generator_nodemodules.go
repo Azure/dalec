67ca97bce0728853bbfebdb343a22f66bcdcb2b0
Brian Goff <cpuguy83@gmail.com>
2026-05-14 14:06:26 -0700

    frontend: add build-time source filters
    Add a source-options context that can provide a YAML source filter
    config at build time. The config currently supports global_excludes
    which can be used to filter out paths from all sources.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>
    (cherry picked from commit 3713d39916c21f4f0c976fcb0f33e8d28c104cce)
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>
    (cherry picked from commit ff5f3f64cfdf2595d70228c042782309d2ac32c4)
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- generator_nodemodules.go
+++ generator_nodemodules.go
@@ -107,7 +107,7 @@ func (s *Spec) NodeModDeps(sOpt SourceOpts, worker llb.State, opts ...llb.Constr
 			}
 			merged = merged.With(withNodeMod(gen, worker, key, opts...))
 		}
-		result[key] = merged
+		result[key] = merged.With(sourceFilterAtPath(sOpt, key, opts...))
 	}
 	return result, nil
 }
